c639b16a41db0643c3d0422221b695e8feb85190
Yusuf Gökhan Kuyu <yusufgokhankuyu@gmail.com>
2023-12-08 00:11:32 +0300

    commit new

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Hava Ne Durumda',
+      title: 'Hava Ne Durumdadır',
       home: WelcomePage(),
     );
   }
---
+++ lib/services/api_service.dart
@@ -0,0 +1,14 @@
+import 'dart:io';
+import 'package:http/http.dart' as http;
+
+class ApiService {
+  static Future getWeatherDataByCity(String city) async {
+    return await http.get(
+        Uri.parse(
+            "https://api.collectapi.com/weather/getWeather?data.lang=tr&data.city=$city"),
+        headers: {
+          HttpHeaders.authorizationHeader: '******',
+          HttpHeaders.contentTypeHeader: 'application/json'
+        });
+  }
+}
